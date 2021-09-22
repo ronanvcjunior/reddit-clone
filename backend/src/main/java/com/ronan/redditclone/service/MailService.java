6b1e0da381738d4b05c65c8bd33c9612f0234ed8
Ronan Vieira <ronanvcjunior@gmail.com>
2021-09-22 02:57:10 -0300

    send email Async

--- backend/src/main/java/com/ronan/redditclone/service/MailService.java
+++ backend/src/main/java/com/ronan/redditclone/service/MailService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +19,7 @@ public class MailService {
     @Autowired
     private MailContentBuilder mailContentBuilder;
     
+    @Async
     public void sendMail(NotificationEmail notificationEmail) {
         MimeMessagePreparator messagePreparator = (mimeMessage) -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
