ba048fd5e7137d4736894fc4ab348e2ad723f63a
Ronan Vieira <ronanvcjunior@gmail.com>
2021-09-24 17:49:47 -0300

    getAllCommentsForPost

--- backend/src/main/java/com/ronan/redditclone/controller/CommentController.java
+++ backend/src/main/java/com/ronan/redditclone/controller/CommentController.java
@@ -1,10 +1,14 @@
 package com.ronan.redditclone.controller;
 
+import java.util.List;
+
 import com.ronan.redditclone.dto.CommentsDto;
 import com.ronan.redditclone.service.CommentService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +27,11 @@ public class CommentController {
     public ResponseEntity<CommentsDto> createComment(@RequestBody CommentsDto commentsDto) {
         commentsDto = service.save(commentsDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(commentsDto);
+    }
+
+    @GetMapping("/by-post/{postId}")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForPost(@PathVariable Long postId) {
+        List<CommentsDto> commentsDtos = service.getAllCommentsForPost(postId);
+        return ResponseEntity.ok().body(commentsDtos);
     }
 }
--- backend/src/main/java/com/ronan/redditclone/service/CommentService.java
+++ backend/src/main/java/com/ronan/redditclone/service/CommentService.java
@@ -1,5 +1,8 @@
 package com.ronan.redditclone.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.ronan.redditclone.domain.Comment;
 import com.ronan.redditclone.domain.Post;
 import com.ronan.redditclone.dto.CommentsDto;
@@ -31,5 +34,13 @@ public class CommentService {
         comment = repository.save(comment);
         CommentsDto save = mapper.mapCommentToDto(comment);
         return save;
+    }
+
+    public List<CommentsDto> getAllCommentsForPost(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("No subreddit found with ID - " + postId));
+        List<Comment> comments = repository.findByPost(post);
+        List<CommentsDto> commentsDtos = comments.stream().map(mapper::mapCommentToDto).collect(Collectors.toList());
+        return commentsDtos;
     }
 }
